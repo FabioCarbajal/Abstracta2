1e3b9b2617399b7d418fbc44ddf0a5240112ed0d
FabioCarbajal <fabio.carbajal@ucsp.edu.pe>
2018-03-24 13:51:58 -0500

    solo string, cifrado y descifrado

---
+++ cifrar2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void invertir()
+{
+	string alfa={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'}; 
+	string alfa2;
+	int i,j,k;
+	
+	for(i=0;i<26;i++)
+	{
+		cout << alfa[i];
+	}
+	cout << endl;
+	
+	for(j=25;j>=0;j--)
+	{
+		cout << alfa[j];
+	}
+	cout << endl;
+	
+	char men[100];
+	cout << "Introducir mensaje: ";
+	cin.getline(men, 100, '\n');
+	cout << "mensaje sin descifrar: " << men;
+	
+}
+
+int main() 
+{
+	invertir();
+	return 0;
+}
+
